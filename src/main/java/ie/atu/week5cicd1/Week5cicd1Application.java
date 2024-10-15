65dd938041075976f3036f13b5bb32d6c0a9fe50
Choon Xiang <g00425056@atu.ie>
2024-10-15 10:12:25 +0100

    Initial commit

---
+++ src/main/java/ie/atu/week5cicd1/Week5cicd1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week5cicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5cicd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5cicd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5cicd1/Week5cicd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5cicd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5cicd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
